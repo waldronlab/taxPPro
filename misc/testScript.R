766e07cced6e7049fbc3a7ab05cc381c19f12c28
sdgamboa <samuel.gamboa.tuz@gmail.com>
2022-09-02 14:39:13 -0400

    add code and files for completeness

--- misc/testScript.R
+++ misc/testScript.R
@@ -1,35 +1,65 @@
-## A simple script file to test code
+
 library(bugphyzz)
-library(dplyr)
 library(purrr)
 
-phys <- map(physiologies(), as_tibble)
+## Import bugphyzz
+phys <- physiologies()
+bugphyzz_taxids <- map(phys, ~ .x$NCBI_ID) |>
+    unlist() |>
+    unique() |>
+    {\(y) y[!is.na(y)]}() |>
+    as.character()
 
-get_dups <- function(df) {
-    double_annotations <- get_double_annotations(df)
-    agreements <- get_agreements(df)
-    conflicts <- get_conflicts(df)
-    all_dups <- list(
-        double_annotation = double_annotations,
-        agreement = agreements,
-        conflict = conflicts
-    ) |>
-        dplyr::bind_rows(.id = 'Duplicate_type')
-    if (!nrow(all_dups)) {
-        return(NULL)
-    } else {
-        return(all_dups)
-    }
-}
+## Get NCBI table and ids
+ncbi_tax <- get_ncbi_taxonomy(force_download = FALSE)
+ncbi_taxids <- ncbi_tax$NCBI_ID
 
-dups_1 <- map(phys, get_duplicates) |>
-    discard(is.null)
+## All ids
+proc_all_ids_fname <-
+    system.file('extdata/proc_all_ids.txt', package = 'taxPPro')
+proc_all_ids <- read.table(proc_all_ids_fname, header = FALSE)[[1]] |>
+    as.character()
 
-dups <- map(phys, get_dups) |>
-    discard(is.null)
+## Exclude all options form the NCBI
+proc_exclude_all_fname <-
+    system.file('extdata/proc_exclude_all.txt', package = 'taxPPro')
+proc_exclude_all <- read.table(proc_exclude_all_fname, header = FALSE)[[1]] |>
+    as.character()
+
+## Exclude informal names
+proc_exclude_inf_fname <-
+    system.file('extdata/proc_exclude_inf.txt', package = 'taxPPro')
+proc_exclude_inf <- read.table(proc_exclude_inf_fname, header = FALSE)[[1]] |>
+    as.character()
+
+## Exclude unclassified
+proc_exclude_uncla_fname <-
+    system.file('extdata/proc_exclude_unclassified.txt', package = 'taxPPro')
+proc_exclude_uncla <- read.table(proc_exclude_uncla_fname, header = FALSE)[[1]] |>
+    as.character()
 
 
-sum(!map_int(dups_1, nrow) == map_int(dups, nrow))
+
+
+sum(bugphyzz_taxids %in% ncbi_taxids)
+sum(bugphyzz_taxids %in% proc_all_ids)
+sum(bugphyzz_taxids %in% proc_exclude_all)
+sum(bugphyzz_taxids %in% proc_exclude_inf)
+sum(bugphyzz_taxids %in% proc_exclude_uncla)
+
+
+missing <- bugphyzz_taxids[!bugphyzz_taxids %in% proc_exclude_uncla]
+
+
+
+
+
+
+phys |>
+    map(~ dplyr::filter(.x,  NCBI_ID == '997346')) |>
+    purrr::discard(~!nrow(.x))
+
+
 
 
 
