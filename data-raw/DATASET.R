d91a5e0373a8b0b1b3de23c6847239cf0432a9cf
sdgamboa <samuel.gamboa.tuz@gmail.com>
2022-09-05 16:47:48 -0400

    add function to import ncbi ids as grouped in the ncbi taxonomy statistics page

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -2,3 +2,45 @@
 
 usethis::use_data(ncbi_direct_download, overwrite = TRUE)
 
+## Include all ids
+proc_all_ids_fname <-
+    system.file('extdata/proc_all_ids.txt', package = 'taxPPro')
+proc_all_ids <- read.table(proc_all_ids_fname, header = FALSE)[[1]] |>
+    as.character()
+
+## Exclude all
+proc_exclude_all_fname <-
+    system.file('extdata/proc_exclude_all.txt', package = 'taxPPro')
+proc_exclude_all <- read.table(proc_exclude_all_fname, header = FALSE)[[1]] |>
+    as.character()
+
+## Exclude informal names
+proc_exclude_inf_fname <-
+    system.file('extdata/proc_exclude_inf.txt', package = 'taxPPro')
+proc_exclude_inf <- read.table(proc_exclude_inf_fname, header = FALSE)[[1]] |>
+    as.character()
+
+## Exclude unclassified
+proc_exclude_uncla_fname <-
+    system.file('extdata/proc_exclude_unclassified.txt', package = 'taxPPro')
+proc_exclude_uncla <- read.table(proc_exclude_uncla_fname, header = FALSE)[[1]] |>
+    as.character()
+
+## Exclude uncultured
+
+
+## Exclude Unclassified and Informal names
+
+
+## Exclude Unclassifed and uncultured
+
+
+## Exclude Informal names and uncultured
+
+
+
+
+
+
+
+
