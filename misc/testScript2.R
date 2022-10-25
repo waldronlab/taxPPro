857d388aee898e5ebac6a478caa2b272e42c4d33
sdgamboa <samuel.gamboa.tuz@gmail.com>
2022-10-25 01:18:42 -0400

    update sandbox

--- misc/testScript2.R
+++ misc/testScript2.R
@@ -1,7 +1,19 @@
 library(bugphyzz)
 library(taxPPro)
+library(dplyr)
+
 aer = physiologies('aerophilicity')[[1]]
 
 aer_plus <- aer |>
     propagate()
+
+count(aer_plus, Evidence)
+
+aer_upstream <- aer |>
+    upstream()
+
+
+x = calcParentScore(strains)
+x |>
+    filter(.data$Rank == 'species')
 
