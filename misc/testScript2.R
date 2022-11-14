cb73cc434b4f9730e7ca7d6333e6969ef7948153
sdgamboa <samuel.gamboa.tuz@gmail.com>
2022-11-14 12:38:33 -0500

    update test script 2

--- misc/testScript2.R
+++ misc/testScript2.R
@@ -1,39 +1,19 @@
 library(bugphyzz)
-library(dplyr)
-library(purrr)
+library(taxPPro)
 
-# phys <- physiologies()
-# duplicates <- phys |>
-#     lapply(function(x) filterData(x, tax.id.type = 'NCBI_ID')) |>
-#     purrr::discard(is.null) |>
-#     lapply(freq2Scores) |>
-#     lapply(getDuplicates) |>
-#     purrr::discard(is.null)
-
+eg_taxon <- 'Lactobacillus sakei'
 aer <- physiologies('aerophilicity')[[1]]
-dup_filtered <- aer |>
+aer[aer$Taxon_name == eg_taxon, ]
+
+aer_filtered <- aer |>
     filterData(tax.id.type = 'Taxon_name') |>
     freq2Scores() |>
     resolveAgreements() |>
     resolveConflicts()
 
-## From duplicates we should get
-b <- getAgreements(dup_filtered)
-c <- getConflicts(dup_filtered)
+a <- getDoubleAnnotations(aer_filtered)
+b <- getAgreements(aer_filtered)
+c <- getConflicts(aer_filtered)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+aer_filtered[aer_filtered$Taxon_name == eg_taxon,]
 
