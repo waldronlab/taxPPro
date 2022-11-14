a44374c6f5a3909f5354bcc2ed8af2fbcea96fa8
sdgamboa <samuel.gamboa.tuz@gmail.com>
2022-11-14 16:14:39 -0500

    update contents of 002_duplicates.R

--- R/002_duplicates.R
+++ R/002_duplicates.R
@@ -342,3 +342,29 @@ removeDuplicates <- function(df) {
         return(df)
     df[!df$Taxon_name %in% unique(dup$Taxon_name),]
 }
+
+
+# helper functions --------------------------------------------------------
+
+#' Choose column for attribute values
+#'
+#' \code{chooseColVal} chooses the column where attribute values are
+#' actually stored.
+#'
+#' @param df A data frame imported with bugphyzz.
+#'
+#' @return A character string.
+#' @export
+#'
+chooseColVal <- function(df) {
+
+    av <- df$Attribute_value
+
+    if(is.logical(av)) {
+        attr_col <- 'Attribute'
+    } else if (is.numeric(av) || is.character(av)) {
+        attr_col <- 'Attribute_value'
+    }
+
+    attr_col
+}
