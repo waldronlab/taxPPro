f29de844561d74d4eaee4235ab8b401b030c31f1
sdgamboa <samuel.gamboa.tuz@gmail.com>
2022-11-30 14:09:21 -0500

    update vignette with numbers

--- misc/testScript4.R
+++ misc/testScript4.R
@@ -1,9 +1,24 @@
-library(tibble)
 
+library(bugphyzz)
+library(purrr)
+library(taxPPro)
+library(dplyr)
 
-df1 <- tribble(
-    ~Taxon_name, ~Attribute, ~Score, ~Parent_taxon_name,
-    'taxon1', 'aerobic', 1, 'Paredwnt',
-    'taxon2', 'anaerobic', 1, 'same_parent',
-    'taxon3', 'f_anaerobic', 1, 'same_parent'
+phys <- physiologies()
+phys <- map(phys, as_tibble)
+remove_dat <- c(
+    'habitat', 'antimicrobial resistance', 'isolation site', 'width',
+    'length', 'disease association',
+    'animal pathogen', 'biofilm forming', 'growth temperature'
 )
+
+# phys <- phys[!names(phys) %in% remove_dat]
+
+phys_char <- phys |>
+    keep(~ is.character(.x$Attribute_value))
+
+phys_char |>
+    map(head) |>
+    bind_rows(.id = 'dataset') |>
+    View()
+
