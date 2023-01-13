8ce56ea6e2ceaa16e6dc61d232649b5216e1b64e
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-01-13 13:40:43 -0500

    update testScript5.R

--- misc/testScript5.R
+++ misc/testScript5.R
@@ -1,9 +1,11 @@
 
 library(bugphyzz)
 library(taxPPro)
-library(dplyr)
 library(data.tree)
-library(purrr)
+library(ete3r)
+library(dplyr)
+
+# First approach ----------------------------------------------------------
 
 valid_ranks <- validRanks()
 ncbi_taxonomy <- get_ncbi_taxonomy() |>
@@ -20,53 +22,43 @@ aer <- physiologies('aerophilicity', remove_false = TRUE)[[1]] |>
     ) |>
     preSteps(tax.id.type = 'NCBI_ID')
 
-data <- left_join(aer, ncbi_taxonomy, by = 'NCBI_ID') |>
+bac_data <- left_join(aer, ncbi_taxonomy, by = 'NCBI_ID') |>
     filter(kingdom == 'Bacteria')
 
-data <- ncbi_taxonomy
-data$pathString <- paste(
-    'k__', data$kingdom,  ## This will be the root
-    '|||p__', data$phylum,
-    '|||c__', data$class,
-    '|||o__', data$order,
-    '|||f__', data$family,
-    '|||g__', data$genus,
-    '|||s__', data$species,
-    '|||t__', data$strain,
+bac_data$pathString <- paste(
+    'k__', bac_data$kingdom,  ## This will be the root
+    '|||p__', bac_data$phylum,
+    '|||c__', bac_data$class,
+    '|||o__', bac_data$order,
+    '|||f__', bac_data$family,
+    '|||g__', bac_data$genus,
+    '|||s__', bac_data$species,
+    '|||t__', bac_data$strain,
     sep = ''
 )
 regex1 <- '(\\|\\|\\|[kpcofgst]__NA)*$'
-data$pathString <- sub(regex1, "", data$pathString)
+bac_data$pathString <- sub(regex1, "", bac_data$pathString)
 regex2 <- '(\\|\\|\\|[kpcofgst]__[^\\|]*$)'
-data$Taxon_name <- stringr::str_extract(data$pathString, regex2) |>
+bac_data$Taxon_name <- stringr::str_extract(bac_data$pathString, regex2) |>
     stringr::str_remove('\\|\\|\\|')
-data_tree <- data |>
+
+bac_data_tree <- bac_data |>
     select(
         kingdom, phylum, class, order, family, genus, species, strain,
         Taxon_name, pathString
     ) |>
     distinct()
 
-bac_data <- data_tree[data_tree$kingdom == 'Bacteria', ]
-arc_data <- data_tree[data_tree$kingdom == 'Archaea',]
-
 start_time <- Sys.time()
-arc_tree <- as.Node(arc_data, pathDelimiter = '|||' )
+bac_tree <- as.Node(bac_data_tree, pathDelimiter = '|||' )
 end_time <- Sys.time()
 difftime(end_time, start_time)
-
-start_time <- Sys.time()
-bac_tree <- as.Node(bac_data, pathDelimiter = '|||' )
-end_time <- Sys.time()
-difftime(end_time, start_time)
-
-
-
-
-tree <- as.Node(data, pathDelimiter = '|||')
+## Stop here
+bac_tree$totalCount
+bac_tree$leafCount
 
 ## Add bugphyzz data
-split_by_taxname <- split(data, factor(data$Taxon_name))
+split_by_taxname <- split(bac_data, factor(bac_data$Taxon_name))
 addBugphyzzAttributes <- function(node, list_of_df) {
     select_cols <- c('NCBI_ID', 'Attribute', 'Evidence', 'Score', 'Frequency')
     node_name <- node$name
@@ -76,12 +68,12 @@ addBugphyzzAttributes <- function(node, list_of_df) {
     }
 }
 
-tree$Do(addBugphyzzAttributes, list_of_df = split_by_taxname)
+# tree$Do(addBugphyzzAttributes, list_of_df = split_by_taxname)
 
 ## Escherichia (genus)
-tree$p__Proteobacteria$c__Gammaproteobacteria$o__Enterobacterales$f__Enterobacteriaceae$g__Escherichia
+length(names(new_tree$p__Proteobacteria$c__Gammaproteobacteria$o__Enterobacterales$f__Enterobacteriaceae$g__Escherichia$children))
 ## Lactobacillus sekei (species)
-tree$p__Firmicutes$c__Bacilli$o__Lactobacillales$f__Lactobacillaceae$g__Latilactobacillus$`s__Latilactobacillus sakei`$`t__Latilactobacillus sakei subsp. sakei 23K`
+bac_tree$p__Firmicutes$c__Bacilli$o__Lactobacillales$f__Lactobacillaceae$g__Latilactobacillus$`s__Latilactobacillus sakei`$`t__Latilactobacillus sakei subsp. sakei 23K`
 
 View(print(tree$p__Proteobacteria$c__Gammaproteobacteria$o__Enterobacterales$f__Enterobacteriaceae$g__Escherichia, 'NCBI_ID', 'Attribute', 'Attribute_value', 'Attribute_source'))
 
@@ -96,8 +88,6 @@ level_names <- data |>
 tree$Climb(
     name = level_names
 )$children[1:3]
-
-
 
 taxizedb::children('Escherichia', db = 'ncbi', verbose = FALSE)
 
@@ -114,3 +104,29 @@ taxizedb::children('Escherichia', db = 'ncbi', verbose = FALSE)
 #
 # taxizedb::children('Bacteria', db = 'ncbi')
 
+# Quick test with ete3r ---------------------------------------------------
+
+ncbi_taxonomy$helper_pathString <- paste(
+    'k__', ncbi_taxonomy$kingdom,  ## This will be the root
+    '|||p__', ncbi_taxonomy$phylum,
+    '|||c__', ncbi_taxonomy$class,
+    '|||o__', ncbi_taxonomy$order,
+    '|||f__', ncbi_taxonomy$family,
+    '|||g__', ncbi_taxonomy$genus,
+    '|||s__', ncbi_taxonomy$species,
+    '|||t__', ncbi_taxonomy$strain,
+    sep = ''
+)
+
+tax <- unique(bac_data$NCBI_ID)
+output_tree <- getTree(taxids = tax)
+data1 <- output_tree@data
+new_names <- unique(data1$name[data1$name != ""])
+
+colnames(ncbi_taxonomy)
+
+x <- ncbi_taxonomy |>
+    filter(NCBI_ID %in% new_names)
+x$pathString <- x$helper_pathString
+
+new_tree <- as.Node(x, pathDelimiter = '|||')
