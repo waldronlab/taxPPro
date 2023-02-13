b39ca727a28e8f1fe4960141065493c606360ff8
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-02-13 10:39:35 -0500

    update script 6

--- misc/testScript6.R
+++ misc/testScript6.R
@@ -3,7 +3,7 @@ library(bugphyzz)
 library(data.tree)
 library(purrr)
 
-## Define function
+## Define functions ####
 fillNAs <- function(x) {
     counter <- 1
     for (i in seq_along(x)) {
@@ -15,55 +15,57 @@ fillNAs <- function(x) {
     return(x)
 }
 
-## Get NCBI data
-ncbi_taxonomy <- get_ncbi_taxonomy()
+taxname2taxid <- function(tax_tbl) {
+
+    if ('kingdom' %in% colnames(tax_tbl)) {
+        pos <- which(colnames(tax_tbl) == 'kingdom')
+        colnames(tax_tbl)[pos] <- 'superkingdom'
+    }
+
+    ranks <- c(
+        'superkingdom', 'phylum', 'class', 'order', 'family', 'genus'
+    )
+    for (i in ranks) {
+        df <- tax_tbl[tax_tbl[['Rank']] == i, ]
+        vct <- df$NCBI_ID
+        names(vct) <- df[[i]]
+        tax_tbl[[i]] <- purrr::map_chr(tax_tbl[[i]], ~ {vct[.x]})
+    }
+    return(tax_tbl)
+}
+
+## Get NCBI data ####
 ncbi_taxids <- get_ncbi_taxids(keyword = 'b', with_taxids = TRUE)
-lgl_vct <- ncbi_taxids$Rank == 'species' & ncbi_taxids$kingdom == 'Bacteria'
-bacteria <- ncbi_taxids[lgl_vct,]
+new_ncbi_taxids <- taxname2taxid(tax_tbl = ncbi_taxids)
+cond1 <- new_ncbi_taxids$Rank == 'species'
+cond2 <- new_ncbi_taxids$superkingdom == '2'
+bacteria <- new_ncbi_taxids[cond1 & cond2,]
 no_select_cols <- c(
-    'strain', 'Taxon_name', 'species', 'Parent_NCBI_ID', 'Rank'
+    'species', 'strain', 'Taxon_name', 'Parent_NCBI_ID', 'Rank'
 )
 bacteria <- bacteria[,!colnames(bacteria) %in% no_select_cols]
 
-## Convert to tree
-# pathString <- paste(
-#     'k__', bacteria$kingdom, # root
-#     '|||p__', bacteria$phylum,
-#     '|||c__', bacteria$class,
-#     '|||o__', bacteria$order,
-#     '|||f__', bacteria$family,
-#     '|||g__', bacteria$genus,
-#     '|||s__', bacteria$NCBI_ID,
-#     sep = ''
-# )
-
-bacteria$NCBI_ID <- paste0('s__', bacteria$NCBI_ID)
 pathString <- paste(
-    bacteria$kingdom, # root
-    '|||', bacteria$phylum,
-    '|||', bacteria$class,
-    '|||', bacteria$order,
-    '|||', bacteria$family,
-    '|||', bacteria$genus,
-    '|||', bacteria$NCBI_ID,
+    'k__', bacteria$superkingdom, # root
+    '|||p__', bacteria$phylum,
+    '|||c__', bacteria$class,
+    '|||o__', bacteria$order,
+    '|||f__', bacteria$family,
+    '|||g__', bacteria$genus,
+    '|||s__', bacteria$NCBI_ID,
     sep = ''
 )
 
-# pathString <- sub(regex1, '', pathString)
 bacteria$pathString <- pathString
 bacteria <- modify(bacteria, fillNAs)
 
-a <- table(bacteria$phylum) |> as.data.frame() |> View()
-
-##
-# phyl <- bacteria[bacteria$phylum == 'Bacillota',]
+## Convert full bacteria data to a data.tree object
 start_time <- Sys.time()
 bac_tree <- as.Node(bacteria, pathDelimiter = '|||')
 end_time <- Sys.time()
 (elapsed_time <- difftime(end_time, start_time))
 
-# bac_tree$Do(addChildren)
-
+print(bac_tree, limit = 20)
 
 
 
