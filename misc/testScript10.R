298f19445f3d45415818a77a79b4570d813b8e8d
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-03-25 03:13:10 -0400

    update script

--- misc/testScript10.R
+++ misc/testScript10.R
@@ -3,15 +3,11 @@ library(bugphyzz)
 library(purrr)
 library(dplyr)
 
-phys_names <- c('aerophilicity', 'growth temperature')
-phys <- physiologies(phys_names, remove_false = TRUE, full_source = TRUE)
-aer <- phys$aerophilicity
-gt <- phys$`growth temperature`
-a <- getDataReadyForPropagation(aer)
-g <- getDataReadyForPropagation(gt)
-a_ag <- getAgreements(a)
-g_ag <- getAgreements(g)
-x <- resolveAgreements(a)
-y <- resolveAgreements(g)
-j <- resolveConflicts(x)
-k <- resolveConflicts(y)
+phys <- physiologies(remove_false = TRUE, full_source = TRUE)
+x <- map(phys, ~ {
+    tryCatch(
+        error = function(e) e, getDataReadyForPropagation(.x)
+    )
+})
+
+any(map_lgl(x, rlang::is_error))
