0179b146c03d1d9f4d65f40e36b09aa9b1c5433e
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-07-01 01:09:42 -0400

    update analysis of NCBI sets

--- R/utils.R
+++ R/utils.R
@@ -79,3 +79,41 @@ scores2Freq <- function(Score) {
 
 ## Function with valid ranks for parents
 .validParentRanks <- function() c('species', 'genus', 'family')
+
+#' Taxize classification to data frame
+#'
+#' \code{classif2Table} converts from classification format (`taxize` packatge)
+#' to a data frame.
+#'
+#' @param x Output from the `taxize::classification` function.
+#' @param ranks Ranks to be selected. Default is given by `.validRanks()`.
+#'
+#' @return A data frame.
+#' @export
+#'
+classif2Table <- function(x, ranks) {
+
+  if (missing(ranks)) {
+    valid_ranks <- .validRanks()
+  } else {
+    valid_ranks <- ranks
+  }
+
+  df_filtered <- x |>
+    dplyr::select(rank, id) |>
+    dplyr::filter(rank %in% valid_ranks)
+
+  new_df <- data.frame(x = df_filtered$id) |>
+    t() |>
+    as.data.frame(row.names = 1L)
+  colnames(new_df) <- df_filtered$rank
+  new_df
+}
+
+
+
+
+
+
+
+
--- inst/scripts/get_tree.R
+++ inst/scripts/get_tree.R
@@ -7,3 +7,37 @@ tree$Do(addStrains)
 tree_list <- as.list(tree)
 usethis::use_data(tree_list, overwrite = TRUE)
 ## starts at 10:05 finished 11:12 (a little more than an hour)
+
+
+library(purrr)
+library(dplyr)
+library(taxPPro)
+library(data.tree)
+
+ranks <- c(
+    'superkingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species',
+    'strain'
+)
+ranks2 <- c('genus', 'species', 'strain')
+fname <- system.file(
+    'extdata/proc_exclude_unclassified_uncultured_06292023.txt',
+    package = 'taxPPro', mustWork = TRUE
+)
+taxids <- read.table(fname, header = FALSE)[[1]]
+taxranks <- taxizedb::taxid2rank(taxids, db = 'ncbi')
+taxdf <- data.frame(id = taxids, rank = taxranks)
+tax_separated <- map(split(taxdf, factor(taxdf$rank)), ~ .x$id)
+gss <- tax_separated[ranks2]
+classification <- map(gss, ~ taxizedb::classification(.x, db = 'ncbi'))
+
+sp <- classification$species
+
+sp_gn <- unique(unlist(map(sp, ~ filter(.x, rank == 'genus')$id)))
+mean(sp_gn %in% gss$genus)
+
+st <- classification$strain
+st_sp <- unique(unlist(map(st, ~ filter(.x, rank == 'species')$id)))
+mean(st_sp %in% gss$species)
+
+st_gn <- unique(unlist(map(st, ~ filter(.x, rank == 'genus')$id)))
+mean(st_gn %in% gss$genus)
