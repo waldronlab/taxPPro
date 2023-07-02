863c959683c6b10db283cc2cdfc1250f5c72510b
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-07-02 14:49:47 -0400

    update data_tree

--- inst/scripts/get_tree.R
+++ inst/scripts/get_tree.R
@@ -1,43 +1,83 @@
-library(taxPPro)
-library(data.tree)
-## The option 'bit' allows having more taxa, only excluding unclassified bacteria
-tbl <- getNCBI(keyword = 'bit')
-tree <- as.Node(tbl[, 'pathString', drop = FALSE], pathDelimiter = '|||')
-tree$Do(addStrains)
-tree_list <- as.list(tree)
-usethis::use_data(tree_list, overwrite = TRUE)
-## starts at 10:05 finished 11:12 (a little more than an hour)
-
-
 library(purrr)
 library(dplyr)
 library(taxPPro)
 library(data.tree)
+library(tidyr)
+library(bugphyzz)
 
-ranks <- c(
-    'superkingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species',
-    'strain'
+fname1 <- system.file(
+    'extdata/proc_exclude_all_06292023.txt',
+    package = 'taxPPro', mustWork = TRUE
 )
-ranks2 <- c('genus', 'species', 'strain')
-fname <- system.file(
+exclude_all_ids <- read.table(fname1, header = FALSE)[[1]]
+exclude_all_ranks <- taxizedb::taxid2rank(exclude_all_ids, db = 'ncbi')
+exclude_all_ids_sp <- exclude_all_ids[which(exclude_all_ranks == 'species')]
+exclude_all_sp_class <- taxizedb::classification(exclude_all_ids_sp, db = 'ncbi')
+exclude_all_sp_tbl <- exclude_all_sp_class |>
+    map(classif2Table) |>
+    bind_rows() |>
+    drop_na() |>
+    filter(superkingdom %in% c('2', '2157'))
+
+fname2 <- system.file(
     'extdata/proc_exclude_unclassified_uncultured_06292023.txt',
     package = 'taxPPro', mustWork = TRUE
 )
-taxids <- read.table(fname, header = FALSE)[[1]]
-taxranks <- taxizedb::taxid2rank(taxids, db = 'ncbi')
-taxdf <- data.frame(id = taxids, rank = taxranks)
-tax_separated <- map(split(taxdf, factor(taxdf$rank)), ~ .x$id)
-gss <- tax_separated[ranks2]
-classification <- map(gss, ~ taxizedb::classification(.x, db = 'ncbi'))
+exclude_uu_ids <- read.table(fname2, header = FALSE)[[1]]
+exclude_uu_ranks <- taxizedb::taxid2rank(exclude_uu_ids, db = 'ncbi')
+exclude_uu_ids_st <- exclude_uu_ids[which(exclude_uu_ranks == 'strain')]
+exclude_uu_st_class <- taxizedb::classification(exclude_uu_ids_st, db = 'ncbi')
+exclude_uu_st_tbl <- exclude_uu_st_class |>
+    map(classif2Table) |>
+    bind_rows() |>
+    drop_na() |>
+    filter(superkingdom %in% c('2', '2157')) |>
+    select(species, strain)
 
-sp <- classification$species
+merged_sp_st <- left_join(
+    exclude_all_sp_tbl, exclude_uu_st_tbl, by = 'species',
+) |>
+    distinct() |>
+    mutate(strain = ifelse(is.na(strain), '', strain))
 
-sp_gn <- unique(unlist(map(sp, ~ filter(.x, rank == 'genus')$id)))
-mean(sp_gn %in% gss$genus)
+phys <- physiologies(remove_false = TRUE, full_source = FALSE)
+bp_ids <- map(phys, ~ .x$NCBI_ID) |>
+    unlist(use.names = FALSE, recursive = TRUE) |>
+    unique() |>
+    {\(y) y[y != 'unknown']}() |>
+    {\(y) y[!is.na(y)]}()
+bp_ranks <- taxizedb::taxid2rank(bp_ids, db = 'ncbi')
+names(bp_ranks) <- bp_ids
+bp_ids_sp <- names(bp_ranks)[which(bp_ranks == 'species')]
 
-st <- classification$strain
-st_sp <- unique(unlist(map(st, ~ filter(.x, rank == 'species')$id)))
-mean(st_sp %in% gss$species)
+extra_ids_sp <- bp_ids_sp[!bp_ids_sp %in% merged_sp_st$species]
+extra_class_sp <- taxizedb::classification(extra_ids_sp, db = 'ncbi')
+extra_tbl_sp <- map(extra_class_sp, classif2Table) |>
+    bind_rows() |>
+    drop_na() |>
+    relocate(
+        superkingdom, phylum, class, order, family, genus, species
+    ) |>
+    filter(superkingdom %in% c('2', '2157')) |>
+    distinct()
 
-st_gn <- unique(unlist(map(st, ~ filter(.x, rank == 'genus')$id)))
-mean(st_gn %in% gss$genus)
+final_tbl <- bind_rows(merged_sp_st, extra_tbl_sp) |>
+    distinct()
+
+df <- data.frame(
+
+    pathString = paste0(
+        'ArcBac|',
+        'd__', final_tbl$superkingdom,
+        '|p__', final_tbl$phylum,
+        '|c__', final_tbl$class,
+        '|o__', final_tbl$order,
+        '|f__', final_tbl$family,
+        '|g__', final_tbl$genus,
+        '|s__', final_tbl$species,
+        '|t__', final_tbl$strain
+    ))
+df$pathString <- sub('\\|t__$', '', df$pathString)
+tree <- as.Node(df[, 'pathString', drop = FALSE], pathDelimiter = '|')
+tree_list <- as.list(tree)
+usethis::use_data(tree_list, overwrite = TRUE)
