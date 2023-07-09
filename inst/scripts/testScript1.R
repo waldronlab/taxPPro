eb51d920631cbe528b3715977d20b36425e67cdf
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-07-09 02:30:10 -0400

    update testScript

--- inst/scripts/testScript1.R
+++ inst/scripts/testScript1.R
@@ -7,3 +7,10 @@
 # binaries <- keep(phys, ~ length(unique(.x$Attribute)) == 1 && !'Attribute_value_max' %in% colnames(.x))
 # ranges <- keep(phys, ~ length(unique(.x$Attribute)) == 1 && 'Attribute_value_max' %in% colnames(.x))
 # categorical <- phys[!names(phys) %in% c(names(binaries), names(ranges))]
+
+library(bugphyzz)
+library(taxPPro)
+phys <- physiologies(full_source = FALSE)
+ready <- lapply(phys, prepareDatForPropagation)
+
+temp <- phys$`growth temperature`
