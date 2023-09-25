5fb5f9db8602635a5244e3054c9ba9df73c41ade
sdgamboa <samuel.gamboa.tuz@gmail.com>
2023-09-24 21:47:56 -0400

    add  code to remove tips with zero length branches since these can cause trouble when running ASR

--- R/trees.R
+++ R/trees.R
@@ -38,16 +38,30 @@ mpa <- function(x = 'tree') {
 #' \code{ltp} gets the LTP tree or data.
 #'
 #' @param x A character string. tree, tips, or nodes. Default is tree.
+#' @param remove_zero_tips Remove or not tips with zero branch lengths.
+#' These tips could cause trouble.
 #'
 #' @return A phylo or data.frame (see the `x` param)
 #' @export
 #'
-ltp <- function(x = 'tree') {
+ltp <- function(x = 'tree', remove_zero_tips = TRUE) {
+    tree_fname <- system.file(
+        'extdata', 'livingTree.newick', package = 'taxPPro'
+    )
+    tree <- ape::read.tree(tree_fname)
+    if (remove_zero_tips) {
+        ntips1 <- length(tree$tip.label)
+        zero_edges <- which(tree$edge.length == 0)
+        nodes_with_zero_edges <- tree$edge[zero_edges, 2]
+        remove_tips <- nodes_with_zero_edges[nodes_with_zero_edges <= length(tree$tip.label)]
+        remove_tips_names <- tree$tip.label[remove_tips]
+        tree <- drop.tip(tree, remove_tips_names)
+        ntips2 <- length(tree$tip.label)
+        message('Dropping ', ntips1 - ntips2, ' tips with zero length branches.')
+        message(ntips2, ' tips remaining in the tree.')
+    }
     if (x == 'tree') {
-        fname <- system.file(
-            'extdata', 'livingTree.newick', package = 'taxPPro'
-        )
-        output <- ape::read.tree(fname)
+        output <- tree
     } else if (x == 'tips') {
         fname <- system.file(
             'extdata', 'livingTree_tips.tsv', package = 'taxPPro'
@@ -56,6 +70,8 @@ ltp <- function(x = 'tree') {
             file = fname, header = TRUE, sep = '\t',
             row.names = NULL
         )
+        rownames(output) <- output$tip_label
+        output <- output[tree$tip.label,]
     } else if (x == 'nodes') {
         fname <- system.file(
             'extdata', 'livingTree_nodes.tsv', package = 'taxPPro'
